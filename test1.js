26e661c0d7186b3f9fbda967363270a5d865d1e9
khanh071006 <donggiakhanh123456@gmail.com>
2025-09-14 00:28:40 +0700

    C1

---
+++ test1.js
@@ -0,0 +1,10 @@
+const today = new Date();
+const currentYear = today.getFullYear();
+
+let birth = 2006;
+let name = "Khanh Dong Gia";
+let isStudent = true;
+
+console.log(`Tên: ${name} 
+Tuổi: ${currentYear - birth}
+Sinh Viên: ${isStudent}`)
