ffbdd2e008d3f38d457306e42730ff01ce9fd101
khanh071006 <donggiakhanh123456@gmail.com>
2025-09-14 09:06:32 +0700

    C3

--- test1.js
+++ test1.js
@@ -1,15 +1,61 @@
-const main = (a, b, c) => {
-    return (a + b + c) / 3;
+const pd1 = {
+    name : "Quat",
+    price : 300,
+    inStock : true
 }
-const rank = ( a ) => {
-    if (a >= 9) 
-        return "Xuat Sac";
-    else if (8 <= a && a <  9)
-        return "Gioi";
-    else if (6.5 <= a && a < 8)
-        return "Kha";
-    else
-        return "Trung binh";
+const pd2 = {
+    name : "Ghe",
+    price : 100,
+    inStock : true
 }
-console.log(main(7,8,9));
-console.log(rank(main(9,7,8)));
+const pd3 = {
+    name : "Ban",
+    price : 400,
+    inStock : false
+}
+const pd4 = {
+    name : "The",
+    price : 30,
+    inStock : true
+}
+const pd5 = {
+    name : "But",
+    price : 10,
+    inStock : false
+}
+const pd6 = {
+    name : "Tu",
+    price : 3000,
+    inStock : true
+};
+
+
+const products = [pd1, pd2, pd3, pd4, pd5];
+
+console.log(pd1.name);
+
+pd2.price = 150;
+console.log(products);
+
+products.push(pd6);
+a = products.slice()
+console.log(a);
+
+products.pop();
+console.log(products);
+
+products.forEach((key,value) => {
+    console.log(key.name);
+})
+
+b = products.map((key,value) =>{
+    return key.price;
+})
+console.log(b);
+
+c = products.filter((key,value) =>{
+    return key.inStock == true;
+})
+console.log(c);
+
+
